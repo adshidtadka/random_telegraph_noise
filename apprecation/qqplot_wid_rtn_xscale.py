b7f9a8cbd0c808a27cd9422e8b85a79c4bd0fd6e
Takaaki Sawa <tsawa@icn.cce.i.kyoto-u.ac.jp>
2018-05-31 17:51:17 +0900

    get max fig hist

--- apprecation/qqplot_wid_rtn_xscale.py
+++ apprecation/qqplot_wid_rtn_xscale.py
@@ -47,26 +47,25 @@ for item in directory:
 	
 	plt.cla()
 	
-	# plt.hist(fmax_ary, 15)
-	# plt.xlabel('$f_\mathrm{max}$ [a.u.]')
-	# plt.ylabel('Population')
+	plt.hist(fmax_ary, 15)
+	plt.xlabel('$f_\mathrm{max}$ [a.u.]')
+	plt.ylabel('Population')
 	
 	#plt.hist(dfof_ary, 20)
 	#plt.xlabel('$\Delta f / f$ [%]')
 	#plt.ylabel('Population')
-	
-	plt.xscale("log")
-	
+
+	# plt.xscale("log")	
 	# plt.plot(wid_pp[0][1], wid_pp[0][0], 'ko', markersize=10, label='WID')
-	plt.plot(rtn_pp[0][1], rtn_pp[0][0], 'b^', markersize=10, label='RTN')
+	# plt.plot(rtn_pp[0][1], rtn_pp[0][0], 'b^', markersize=10, label='RTN')
 	
 	# plt.plot(fmax_ary, dfof_ary, 'ko', markersize=12)
 	
-	plt.xlabel('$f_\mathrm{max}$ [a.u.]')
-	plt.ylabel('$\Delta f / f$ [%]')
+	# plt.xlabel('$f_\mathrm{max}$ [a.u.]')
+	# plt.ylabel('$\Delta f / f$ [%]')
 	
-	print('../fig/' + item[:-4] + '.pdf')
-	plt.savefig('../fig/' + item[:-4] + '.pdf')
+	print('../fig_max_hist/' + item[:-4] + '_fig_max_hist.pdf')
+	plt.savefig('../fig_max_hist/' + item[:-4] + '_fig_max_hist.pdf')
 
 	#plt.legend()
 	#plt.grid()
